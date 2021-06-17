386c24a815b58e321e84e8627177c1ee9822d3ae
ducquy2200 <ducquy2200@gmail.com>
2021-06-17 19:55:46 -0400

    update main.dart - note app

--- take_note/lib/main.dart
+++ take_note/lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TakeNote',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.purple,
 
       ),
-      home: HomePage(),
+      home: new HomePage(),
       
     );
   }
@@ -28,11 +28,100 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> notes = [];
+
+  void _addNewNote(String task){
+    if(task.length > 0) {
+      setState(() => notes.add(task));
+    }
+  }
+
+  void _deleteNote(int index){
+    setState(() => notes.removeAt(index));
+  }
+
+  Widget _buildNotes(){
+    return new ListView.builder(
+      itemBuilder: (context, index){
+        if(index < notes.length){
+          return _buildNote(notes[index], index);
+        }else{
+          return _buildNote("", -1);
+        }
+      }
+    );
+  }
+
+  Widget _buildNote(String noteText, int index){
+    return new ListTile(
+      title: new Text(noteText),
+      onTap:() => _removeNote(index)
+    );
+  }
+
+  void _addNewNoteScreen() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                      title: new Text('Add a new note')
+                  ),
+                  body: new TextField(
+                    autofocus: true,
+                    decoration: new InputDecoration(
+                        hintText: 'Enter note here ...',
+                        contentPadding: const EdgeInsets.all(16.0)
+                    ),
+                    keyboardType: TextInputType.text,
+                    maxLines: null,
+                    onSubmitted: (val) {
+                      _addNewNote(val);
+                      Navigator.pop(context); // Close the add todo screen
+                    },
+                  )
+              );
+            }
+        )
+    );
+  }
+
+  void _removeNote(int index){
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+              title: new Text('Are you sure you want to delete note #${index + 1} ?'),
+              actions: <Widget>[
+                new TextButton(
+                    child: new Text('CANCEL'),
+                    onPressed: () => Navigator.of(context).pop()
+                ),
+                new TextButton(
+                    child: new Text('DELETE'),
+                    onPressed: () {
+                      _deleteNote(index);
+                      Navigator.of(context).pop();
+                    }
+                )
+              ]
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-      
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Take Note Homepage"),
+        ),
+        body: _buildNotes(),
+        floatingActionButton: new FloatingActionButton(
+            onPressed: _addNewNoteScreen,
+            tooltip: 'Add note',
+            child: new Icon(Icons.add),
+        ),
     );
   }
 }
