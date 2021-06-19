bd613f008c165c24b42358bf14417ad75a3a2f48
Grace F <66697045+grace-ful@users.noreply.github.com>
2021-06-18 21:37:22 -0400

    added icon and text colors

--- take_note/lib/main.dart
+++ take_note/lib/main.dart
@@ -12,16 +12,14 @@ class MyApp extends StatelessWidget {
       title: 'TakeNote',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-
       ),
       home: new HomePage(),
-
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -41,24 +39,24 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildNotes() {
-    return new ListView.builder(
-        itemBuilder: (context, index) {
-          if (index < notes.length) {
-            return _buildNote(notes[index], index);
-          } else {
-            return _buildNote("", -1);
-          }
-        }
-    );
+    return new ListView.builder(itemBuilder: (context, index) {
+      if (index < notes.length) {
+        return _buildNote(notes[index], index);
+      } else {
+        return _buildNote("", -1);
+      }
+    });
   }
 
   Widget _buildNote(String noteText, int index) {
     if (noteText != "") {
       return new ListTile(
         title: new Text(noteText),
-        trailing: IconButton(icon: Icon(Icons.delete), onPressed: () {
-          _removeNote(index);
-        }),
+        trailing: IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              _removeNote(index);
+            }),
       );
     }
     return new ListTile(
@@ -67,40 +65,35 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _addNewNoteScreen() {
-    Navigator.of(context).push(
-        new MaterialPageRoute(
-            builder: (context) {
-              return new Scaffold(
-                  appBar: new AppBar(
-                    title: TextField(
-                      autocorrect: true,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        hintText: 'New Note',
-                      ),
-                      style: TextStyle(color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22),
-                    ),
-                    backgroundColor: Colors.purple,
-                  ),
-                  body: new TextField(
-                    autofocus: true,
-                    decoration: new InputDecoration(
-                        hintText: 'Enter note here ...',
-                        contentPadding: const EdgeInsets.all(16.0)
-                    ),
-                    keyboardType: TextInputType.text,
-                    maxLines: null,
-                    onSubmitted: (val) {
-                      _addNewNote(val);
-                      Navigator.pop(context); // Close the add todo screen
-                    },
-                  )
-              );
-            }
-        )
-    );
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: new AppBar(
+            title: TextField(
+              autocorrect: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                hintText: 'New Note',
+              ),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22),
+            ),
+            backgroundColor: Colors.purple,
+          ),
+          body: new TextField(
+            autofocus: true,
+            decoration: new InputDecoration(
+                hintText: 'Enter note here ...',
+                contentPadding: const EdgeInsets.all(16.0)),
+            keyboardType: TextInputType.text,
+            maxLines: null,
+            onSubmitted: (val) {
+              _addNewNote(val);
+              Navigator.pop(context); // Close the add todo screen
+            },
+          ));
+    }));
   }
 
   void _removeNote(int index) {
@@ -113,76 +106,91 @@ class _HomePageState extends State<HomePage> {
               actions: <Widget>[
                 new TextButton(
                     child: new Text('CANCEL'),
-                    onPressed: () => Navigator.of(context).pop()
-                ),
+                    onPressed: () => Navigator.of(context).pop()),
                 new TextButton(
                     child: new Text('DELETE'),
                     onPressed: () {
                       _deleteNote(index);
                       Navigator.of(context).pop();
-                    }
-                )
-              ]
-          );
-        }
-    );
+                    })
+              ]);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Take Note "),
-        ),
-        body: _buildNotes(),
-        floatingActionButton: new FloatingActionButton(
-          onPressed: _addNewNoteScreen,
-          tooltip: 'Add note',
-          child: new Icon(Icons.add),
-        ),
-        drawer: Drawer(
-          child: Container(
-            color: Colors.white60,
-            child: ListView(
-              padding: EdgeInsets.all(5.0),
-              children: <Widget>[
-                DrawerHeader(
-                    child: Text('TakeNote'),
+      appBar: new AppBar(
+        title: new Text("Take Note "),
+      ),
+      body: _buildNotes(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addNewNoteScreen,
+        tooltip: 'Add note',
+        child: new Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.white70,
+          child: ListView(
+            padding: EdgeInsets.all(5.0),
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('TakeNote'),
+              ),
+              ListTile(
+                leading: Icon(Icons.home_outlined, color: Colors.purple[800],),
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    color: Colors.purple[800],
+                  ),
                 ),
-                ListTile(
-                  leading: Icon(Icons.home_outlined),
-                  title: Text('Home'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.star_border_outlined, color: Colors.purple[800]),
+                title: Text(
+                  'Favourites',
+                  style: TextStyle(
+                    color: Colors.purple[800],
+                  ),
                 ),
-                Divider(),
-                ListTile(
-                  leading: Icon(Icons.star_border_outlined),
-                  title: Text('Favourites'),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.archive_outlined, color: Colors.purple[800],),
+                title: Text(
+                  'Archive',
+                  style: TextStyle(
+                    color: Colors.purple[800],
+                  ),
                 ),
-                Divider(),
-                ListTile(
-                  leading: Icon(Icons.archive_outlined),
-                  title: Text('Archive'),
-                  onTap:() {
-                    Navigator.pop(context);
-                  },
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.settings_outlined, color: Colors.purple[800],),
+                title: Text(
+                  'Settings',
+                  style: TextStyle(
+                    color: Colors.purple[800],
+                  ),
                 ),
-                Divider(),
-                ListTile(
-                  leading: Icon(Icons.settings_outlined),
-                  title: Text('Settings'),
-                ),
-                Divider(),
-              ],
-            ),
+              ),
+              Divider(),
+            ],
           ),
         ),
+      ),
     );
   }
-
 }
